250c27456a2629939bdb8365d6ed2c57006df1a2
Pierre van Male <vmalep@protonmail.com>
2024-02-21 10:53:05 +0100

    code for gallery moved to js file

--- views/galeria.js
+++ views/galeria.js
@@ -6,6 +6,18 @@ class Galeria extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
       <h1>Galerìa</h1>
+      <!-- Carousel wrapper -->
+      <div class="carousel-inner py-4">
+          <!-- Single item -->
+          <div class="carousel-item active">
+              <div class="container">
+                  <!-- <div class="row"> -->
+                  <div id="myGalleryCards" class="row"></div>
+                  <!-- </div> -->
+              </div>
+          </div>
+      </div>
+
     `;
   }
 }
